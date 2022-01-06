55d871a6303f8d0db7922276d83e8d9a4f7cf709
mpsloan <97258032+mpsloan@users.noreply.github.com>
2022-01-06 16:40:31 -0500

    Add files via upload

---
+++ SimonSays.java
@@ -0,0 +1,171 @@
+/*
+Michael Sloan
+1180-02 / 1180L-06
+Wallace Neikirk / Luke Holt
+October 8, 2021
+This program asks the user which difficulty level they would like to play at and denies any answer besides
+easy, medium, or hard. Easy mode prints out an ever increasing list of random colors: red, blue, green and yellow.
+Medium mode prints out an ever increasing list of random numbers 1-9. Hard mode prints out and ever increasing
+list of random lowercase characters. The answers are marked correct regardless of spacing and capitalization and
+the users score is output after each correct answer. Once the user gets the question wrong, the program outputs 
+the users final score, and gives them the option to play again. If the user chooses yes they are prompted to
+choose their difficulty level again and if they give and invalid answer they will be prompted with the same 
+question regarding whether they would like to play again.
+*/
+import java.util.Scanner;
+import java.util.Random;
+
+public class SimonSays {
+    public static void main(String[] args) throws Exception {
+        Scanner fetch = new Scanner (System.in);
+        String playAgain;
+        //initializing a variable to break out of do-while loop
+        int breakOut = 0;
+        //initializing lists for the separate levels to add the random sequences to
+        String colorList = "";
+        String numberList = "";
+        String letterList = "";
+
+        //do while loop so you are able to replay if you choose to
+        do {
+            System.out.println("Which difficulty level do you want: easy, medium, or hard?");
+            String level = fetch.nextLine();
+            int score = 0;
+            //allowing user to choose difficulty level with conditionals
+            if (level.equalsIgnoreCase("easy")) {
+                String colors[] = {"Red", "Green", "Yellow", "Blue"};
+                //for loop to add additional random values to sequence if answer is correct
+                for (int j = 0; score == j; j++) {
+                        Random random = new Random();
+                        int randomColor = random.nextInt(colors.length);
+                        //adding the colors to the color list string
+                        colorList += colors[randomColor] + " ";
+                        System.out.println("Simon says: " + colorList);
+                    //2 second pause and then clears the terminal for 200 spaces
+                    Thread.sleep(2000);
+                    for (int k = 0; k <= 200; k++ ) {
+                        System.out.println(" ");
+                    }
+                    String easyAnswer = fetch.nextLine();
+                    //ignoring casing and spaces
+                    if (easyAnswer.replace(" ", "").equalsIgnoreCase(colorList.replace(" ", ""))) {
+                        score +=1;
+                        System.out.println("Right! Your score is " +score+ ".");
+                    }
+
+                    else {
+                        System.out.println("Incorrect! Your score for this round was " +score+ ".");
+                        //loop that repeats if the user types in an invalid answer, set to 100 just as an arbitrary number
+                        for (int l = 0; l <= 100; l++) {
+                            System.out.println("Do you want to play again (yes or no)?");
+                            playAgain = fetch.nextLine();
+                            if (playAgain.equalsIgnoreCase("yes")) {
+                                System.out.println("Good choice!");
+                                break;
+                            }
+                            else if (playAgain.equalsIgnoreCase("no")) {
+                                System.out.print("Ok, goodbye!");
+                                //exit out of do while loop
+                                breakOut = 1;
+                                break;
+                            }
+                            else {
+                                System.out.println("That is not a valid answer!");
+                            }
+                        }
+                    }
+                }
+            }
+            //medium mode is formatted similarly to easy
+            else if (level.equalsIgnoreCase("medium")) {
+                //array of numbers 1-9
+                String numbers[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+                Random random2 = new Random();
+                for (int m = 0; score == m; m++) {
+                    int randomNumber = random2.nextInt(numbers.length);
+                    numberList += numbers[randomNumber] + " ";
+                    System.out.println("Simon says: " + numberList);
+
+                    Thread.sleep(2000);
+                    for (int l = 0; l <= 200; l++) {
+                        System.out.println(" ");
+                    }
+                    String mediumAnswer = fetch.nextLine();
+                    if (mediumAnswer.replace(" ", "").equalsIgnoreCase(numberList.replace(" ", ""))) {
+                        score += 1;
+                        System.out.println("Right! Your score is " +score+ ".");
+                    }
+                    else {
+                        System.out.println("Incorrect! Your score for this round was " +score+ ".");
+                        for (int n = 0; n <= 100; n++) {
+                            System.out.println("Do you want to play again (yes or no)?");
+                            playAgain = fetch.nextLine();
+                            if (playAgain.equalsIgnoreCase("yes")) {
+                                System.out.println("Good choice!");
+                                break;
+                            }
+                            else if (playAgain.equalsIgnoreCase("no")) {
+                                System.out.print("Ok, goodbye!");
+                                breakOut = 1;
+                                break;
+                            }
+                            else {
+                                System.out.println("That is not a valid answer!");
+                            }
+                        }
+                    }
+                }
+            }
+            //hard mode is formatted similarly to easy and medium
+            else if (level.equalsIgnoreCase("hard")) {
+                //array of all possible letters
+                String letters[] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
+                        "n","o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+                Random random3 = new Random();
+                for (int o = 0; score == o; o++) {
+                    int randomLetter = random3.nextInt(letters.length);
+                    letterList += letters[randomLetter] + " ";
+                    System.out.println("Simon says: " + letterList);
+                    Thread.sleep(2000);
+                    for (int n = 0; n <=200; n++) {
+                        System.out.println(" ");
+                    }
+                    String hardAnswer = fetch.nextLine();
+                    if (hardAnswer.replace(" ", "").equalsIgnoreCase(letterList.replace(" ", ""))) {
+                        score += 1;
+                        System.out.println("Right! Your score is " +score+ ".");
+                    }
+                    else {
+                        System.out.println("Incorrect! Your score for this round was " +score+ ".");
+                        for (int p = 0; p <= 100; p++) {
+                            System.out.println("Do you want to play again (yes or no)?");
+                            playAgain = fetch.nextLine();
+                            if (playAgain.equalsIgnoreCase("yes")) {
+                                System.out.println("Good choice!");
+                                break;
+                            }
+                            else if (playAgain.equalsIgnoreCase("no")) {
+                                System.out.print("Ok, goodbye!");
+                                breakOut = 1;
+                                break;
+                            }
+                            else {
+                                System.out.println("That is not a valid answer!");
+                            }
+                        }
+
+
+
+
+                    }
+                }
+            }
+            else {
+                //invalid difficulty level response if user answer isn't easy, medium or hard
+                System.out.println("That is not a valid difficulty level.");
+            }
+        }
+        while (breakOut <= 0);
+        fetch.close();
+    }
+}
